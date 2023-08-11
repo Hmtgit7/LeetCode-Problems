9676213803ae74848024a11f23d55deee993b257
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-08-11 22:47:57 +0530

    leetcode 1018

---
+++ C++ Solutions/BinaryPrefixDivisibleByFive1018.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<bool> prefixesDivBy5(vector<int> &nums)
+{
+    vector<bool> res;
+    int num = nums[0];
+    bool dBFive = num % 5 == 0 ? true : false;
+    res.push_back(dBFive);
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[i] == 1)
+        {
+            num = num * 2 + 1;
+        }
+        else
+        {
+            num = num * 2;
+        }
+        if (num % 5 == 0)
+            res.push_back(true);
+        if (num % 5 != 0)
+            res.push_back(false);
+        num = num % 5;
+    }
+    return res;
+}
+int main()
+{
+    vector<int> nums = {0, 1, 1, 1, 1, 1};
+    vector<bool> res = prefixesDivBy5(nums);
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << " ";
+    }
+    return 0;
+}
